0902bc40c1b4ca5415e6712c6620ee93d2f7ec79
btiac1129 <btiac1129@gmail.com>
2020-10-08 15:42:08 +0900

    create Town, House, Condition models

--- Lighttown/models.py
+++ Lighttown/models.py
@@ -1,21 +1,34 @@
 from django.db import models
+from django.conf import settings
+from datetime import timedelta
 
 class Town(models.Model):
     name = models.CharField(max_length=50)
     description = models.CharField(max_length=140)
+    operator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["name"]
 
     def __str__(self):
         return self.name
 
+
 class House(models.Model):
-    town = models.ForeignKey(Town, on_delete=models.SET()) # FIXME:
+    town = models.ForeignKey(Town, on_delete=models.CASCADE)
+    town_name = models.JSONField(null=True, blank=True) # FIXME:
     title = models.CharField(max_length=50)
     description = models.CharField(max_length=140)
-    date_started = models.DateTimeField()
-    date_finished = models.DateTimeField()
-    is_completed = models.BooleanField()
-    created_at = models.DateTimeField(auto_now=True, auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True, auto_now_add=True)
+    date_started = models.DateTimeField(blank=True)
+    date_finished = models.DateTimeField(blank=True)
+    session = models.DurationField(default=timedelta(days=1))
+    is_completed = models.BooleanField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    builder = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ["title"]
 
     def __str__(self):
         return self.title 
@@ -24,3 +37,5 @@ class Condition(models.Model):
     title = models.ForeignKey(House, on_delete=models.CASCADE)
     condition = models.CharField(max_length=300)
 
+    class Meta:
+        ordering = ["id"]
